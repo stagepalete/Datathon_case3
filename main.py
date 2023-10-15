d974f295f903e6c44980f6ce95f1fc5822bf6c71
Kaptela <132116843+Kaptela@users.noreply.github.com>
2023-10-15 14:41:18 +0600

    Update main.py
    I add first model structure

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from keras.layers import Conv2D, Dense, Flatten, MaxPool2D, BatchNormalization, Dropout
 from keras.models import Sequential
 from keras.preprocessing.image import ImageDataGenerator
-from keras.mixed_precision import experimental as mixed_precision
-
-policy = mixed_precision.Policy('mixed_float16')
-mixed_precision.set_policy(policy)
+from keras.callbacks import ReduceLROnPlateau
 
 train_datagen = ImageDataGenerator(
     rescale=1/255,
     validation_split=0.2,
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
 )
 
 train_generator = train_datagen.flow_from_directory(
     directory='techosmotr/train',
-    target_size=(250, 250),
+    target_size=(160, 160),
     batch_size=32,
     seed=42,
     class_mode='categorical',
@@ -22,7 +26,7 @@ train_generator = train_datagen.flow_from_directory(
 
 validation_generator = train_datagen.flow_from_directory(
     directory='techosmotr/train',
-    target_size=(250, 250),
+    target_size=(160, 160),
     batch_size=32,
     seed=42,
     class_mode='categorical',
@@ -31,27 +35,20 @@ validation_generator = train_datagen.flow_from_directory(
 
 model = Sequential()
 
-model.add(Conv2D(input_shape=(250, 250, 3), filters=32, kernel_size=(3, 3), padding='same', activation='relu'))
+model.add(Conv2D(input_shape=(160, 160, 3), filters=32, kernel_size=(3, 3), padding='same', activation='relu'))
 model.add(Conv2D(filters=32, kernel_size=(3, 3), padding='same', activation='relu'))
 model.add(MaxPool2D(pool_size=(2, 2)))
-# model.add(Dropout(0.25))
+model.add(Dropout(0.25))
 
 model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu'))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu'))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu'))
 model.add(MaxPool2D(pool_size=(2, 2)))
-model.add(Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='relu'))
-model.add(MaxPool2D(pool_size=(2, 2)))
-model.add(Conv2D(filters=256, kernel_size=(3, 3), padding='same', activation='relu'))
-model.add(MaxPool2D(pool_size=(2, 2)))
-model.add(Conv2D(filters=512, kernel_size=(3, 3), padding='same', activation='relu'))
-model.add(Conv2D(filters=512, kernel_size=(3, 3), padding='same', activation='relu'))
-model.add(MaxPool2D(pool_size=(2, 2)))
-model.add(MaxPool2D(pool_size=(2, 2)))
-
+model.add(Dropout(0.25))
 
 model.add(Flatten())
 
-model.add(Dense(units=1024, activation='relu'))
-model.add(Dense(units=256, activation='relu'))
+model.add(Dense(units=600, activation='relu'))
 model.add(Dense(units=5, activation='softmax'))
 
 print(model.summary())
@@ -62,12 +59,13 @@ model.compile(
     metrics=['accuracy']
 )
 
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=1e-6)
+
 model_fit = model.fit(
     train_generator,
-    epochs=12,
+    epochs=100,
     steps_per_epoch=train_generator.samples // 32,
     validation_data=validation_generator,
     validation_steps=validation_generator.samples // 32,
+    callbacks=[reduce_lr]
 )
-
-model.save('model3')
